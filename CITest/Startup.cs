32a040422601d3606202b016e3e929bdd20aa2ac
m.pobezhimova <m.pobezhimova@itransition.com>
2017-09-29 16:39:06 +0300

    initial project setup

---
+++ CITest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CITest.Startup))]
+namespace CITest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
